e5af963c74671feea0ce898afaafaf12c1c32277
trunghoangnt2003 <120396322+trunghoangnt2003@users.noreply.github.com>
2024-05-10 11:39:58 +0700

    done

--- marketplace/src/utils/is-authenticated.tsx
+++ marketplace/src/utils/is-authenticated.tsx
@@ -1,3 +1,26 @@
-export function isAuthenticated() {
-    return !!localStorage.getItem("token");
+export const isAuthenticated =()=> {
+    const token = localStorage.getItem("token");
+    if ( token === null)
+    {
+        return false;
+    }else {
+        return !isTokenExpired(token);
+    }
+}
+
+function isTokenExpired(token) {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+
+  const { exp } = JSON.parse(jsonPayload);
+  const expired = Date.now() >= exp * 1000
+  return expired
 }
