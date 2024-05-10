1cd817a6204388d56fa05230653f2849afe08591
trunghoangnt2003 <120396322+trunghoangnt2003@users.noreply.github.com>
2024-05-10 16:37:40 +0700

    boder

--- marketplace/src/components/common/inventory/filter.tsx
+++ marketplace/src/components/common/inventory/filter.tsx
@@ -18,14 +18,16 @@ export const FilterInventory = ({name,actionValues, eventSearch, handleChange}) 
                           },
                     },
                     "& .MuiInputBase-root": {
-                        color: "#F1E9DC", // Màu text của Select khi không focus
+                        color: "#F1E9DC",
+                        //border: "2px solid #B7A284", // Đường viền 2px với màu sắc #B7A284
+                        backgroundColor: "#170A02CC", // Màu nền của FormControl // Màu text của Select khi không focus
                     },
 
                     "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline":
                         {
                             borderColor: "#B7A284",
                         },
-                        backgroundColor: "#170A02CC",
+                        
                         "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
                             borderColor: "orange", // Màu border khi focus là màu cam
                           },
